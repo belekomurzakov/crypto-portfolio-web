21c79416b81f94d2766e3b032fde8035500e68e4
Belek Omurzakov <omurzakovb@gmail.com>
2022-06-18 16:21:30 +0200

    API implemented

--- views/prices.py
+++ views/prices.py
@@ -1,5 +1,16 @@
 from flask import Blueprint, render_template
+
 bp = Blueprint('prices', __name__, url_prefix='/prices')
+import requests
+import json
+
+
+@bp.route('/', methods=['GET'])
+def get_data():
+    req = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page'
+                       '=250&page=1&sparkline=true&price_change_percentage=24h')
+    data = json.loads(req.content)
+    return render_template('prices/prices.html', data=data)
 
 
 @bp.route('/')
